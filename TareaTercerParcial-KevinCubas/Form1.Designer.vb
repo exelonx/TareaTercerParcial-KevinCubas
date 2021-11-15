800cb13a4d981d44771a14a7622c98d38a4bc93b
Kevin Cubas <kevin.otaku@hotmail.com>
2021-11-14 21:14:15 -0600

    Agregar archivos de proyecto.

---
+++ TareaTercerParcial-KevinCubas/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.busqueda = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.filaPalabra = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.columnaPalabra = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'busqueda
+        '
+        Me.busqueda.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.busqueda.Location = New System.Drawing.Point(419, 35)
+        Me.busqueda.Name = "busqueda"
+        Me.busqueda.Size = New System.Drawing.Size(245, 26)
+        Me.busqueda.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(415, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Palabra a buscar"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(415, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Fila"
+        '
+        'filaPalabra
+        '
+        Me.filaPalabra.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.filaPalabra.Location = New System.Drawing.Point(564, 83)
+        Me.filaPalabra.Name = "filaPalabra"
+        Me.filaPalabra.ReadOnly = True
+        Me.filaPalabra.Size = New System.Drawing.Size(100, 26)
+        Me.filaPalabra.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(415, 121)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Columna"
+        '
+        'columnaPalabra
+        '
+        Me.columnaPalabra.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.columnaPalabra.Location = New System.Drawing.Point(564, 118)
+        Me.columnaPalabra.Name = "columnaPalabra"
+        Me.columnaPalabra.ReadOnly = True
+        Me.columnaPalabra.Size = New System.Drawing.Size(100, 26)
+        Me.columnaPalabra.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(511, 202)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 36)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeColumns = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8, Me.Column9, Me.Column10})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(397, 248)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = ""
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Width = 35
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = ""
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 35
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = ""
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        Me.Column3.Width = 35
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = ""
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        Me.Column4.Width = 35
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = ""
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        Me.Column5.Width = 35
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = ""
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        Me.Column6.Width = 35
+        '
+        'Column7
+        '
+        Me.Column7.HeaderText = ""
+        Me.Column7.Name = "Column7"
+        Me.Column7.ReadOnly = True
+        Me.Column7.Width = 35
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = ""
+        Me.Column8.Name = "Column8"
+        Me.Column8.ReadOnly = True
+        Me.Column8.Width = 35
+        '
+        'Column9
+        '
+        Me.Column9.HeaderText = ""
+        Me.Column9.Name = "Column9"
+        Me.Column9.ReadOnly = True
+        Me.Column9.Width = 35
+        '
+        'Column10
+        '
+        Me.Column10.HeaderText = ""
+        Me.Column10.Name = "Column10"
+        Me.Column10.ReadOnly = True
+        Me.Column10.Width = 35
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(676, 268)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.columnaPalabra)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.filaPalabra)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.busqueda)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents busqueda As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents filaPalabra As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents columnaPalabra As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As DataGridViewTextBoxColumn
+    Friend WithEvents Column10 As DataGridViewTextBoxColumn
+End Class
---
+++ TareaTercerParcial-KevinCubas/Form1.vb
@@ -0,0 +1,85 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Valida espacio en blanco
+        If busqueda.Text = Nothing Then
+            Exit Sub
+        End If
+
+        Try
+            Dim lector As New StreamReader(TareaTercerParcial_KevinCubas.My.Computer.FileSystem.CurrentDirectory & "\tabla.txt")
+            Dim lineaTexto As String = ""
+            Dim fila As Integer = 0         'Indica en que fila se encuentra la palabra buscada
+            Dim columna As Integer = 0      'Indica en que columna se encuentra la palabra buscada
+            Dim match As Integer = 0        'Cuenta cada vez que haya un match de letras
+            Dim lineaIteracion As Integer = 0
+
+            'limpiar datagrid y textbox de filas y columnas
+            DataGridView1.ClearSelection()
+            filaPalabra.Clear()
+            columnaPalabra.Clear()
+
+            While lector.Peek > 0
+                lineaTexto = lector.ReadLine
+                match = 0 'Reiniciar el contador para validar 
+                For i = 0 To lineaTexto.ToArray.Length - 1    'Recorre cada letra de la linea de texto
+                    'Letra de la tabla = Letra de la palabra a buscar
+                    If lineaTexto.Substring(i, 1).ToLower() = LTrim(busqueda.Text.Substring(match, 1).ToLower()) Then
+                        match += 1  'Incrementa el numero de matches (se usa en la comparación de letras)
+                        'Comprueba si el número de matches es igual al número de letras de la palabra a buscar
+                        If busqueda.Text.Length = match Then
+                            fila = lineaIteracion       'Recibe el número de fila
+                            columna = (i + 1) - match   'Calcula la posición de columna
+
+                            'Despliega las filas y columnas
+                            filaPalabra.Text = fila
+                            columnaPalabra.Text = columna
+
+                            'Marca la palabra en el DataGridView
+                            For j = columna To columna + match - 1
+                                DataGridView1.Rows(fila).Cells(j).Selected = True
+                            Next
+                            lector.Close()          'Cierra el streamReader si el match se completa
+                            Exit Sub                'Cierra el ciclo si el match se completa
+                        End If
+                    Else
+                        match = 0       'Reinicia el número de matches para que vuelva a comparar con la primera letra de la palabra a buscar
+                    End If
+                Next
+                lineaIteracion += 1
+            End While
+
+            'En caso de no encontrar un match completo
+            lector.Close()              'Cierra el streamReader si el match NO se completa
+            MsgBox("Palabra no encontrada")
+        Catch ex As Exception
+            MsgBox("Error " & vbCrLf & ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Despliega el archivo en el datagrid
+        Try
+            Dim lector As New StreamReader(TareaTercerParcial_KevinCubas.My.Computer.FileSystem.CurrentDirectory & "\tabla.txt")
+            Dim lineaTexto As String = ""
+            Dim linea As Integer = 0        'Contador para iterar
+
+            While lector.Peek > 0
+                DataGridView1.Rows.Add()
+                lineaTexto = lector.ReadLine
+                For columna = 0 To lineaTexto.ToArray.Length - 1  'Recorrera la distancia de la cadena
+                    'agrega la letra que esta en esta posición al datagrid
+                    DataGridView1.Rows(linea).Cells(columna).Value = lineaTexto.Substring(columna, 1)
+                Next
+                linea += 1  'Pasa a la siguiente fila
+            End While
+            lector.Close()
+        Catch ex As Exception
+            MsgBox("Error " & vbCrLf & ex.Message)
+        End Try
+
+        DataGridView1.ClearSelection()
+
+    End Sub
+End Class
